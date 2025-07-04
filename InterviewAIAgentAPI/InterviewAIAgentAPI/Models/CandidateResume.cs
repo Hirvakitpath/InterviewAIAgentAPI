1457aaf707cade4a2c66e6433967b698bfdb7adf
Jignesh Gohel <jigneshgohel.itpath@gmail.com>
2025-07-04 13:07:39 +0530

    - inint commit 2

---
+++ InterviewAIAgentAPI/InterviewAIAgentAPI/Models/CandidateResume.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewAIAgentAPI.Models
+{
+    public class CandidateResume
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ResumeText { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ InterviewAIAgentAPI/InterviewAIAgentAPI/Models/TwilioSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace InterviewAIAgentAPI.Models
+{
+    public class TwilioSettings
+    {
+        public string AccountSid { get; set; }
+        public string ApiKeySid { get; set; }
+        public string ApiKeySecret { get; set; }
+    }
+}
