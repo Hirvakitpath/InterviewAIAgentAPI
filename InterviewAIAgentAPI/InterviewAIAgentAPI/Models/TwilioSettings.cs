dccfc29bfd2155af7773b1bccfa17876ec460a25
Jignesh Gohel <jigneshgohel.itpath@gmail.com>
2025-07-04 12:49:15 +0530

    Revert "- init commit"
    This reverts commit 6fe7bc6b3c533aae55bd60cc9d9dc048a8911d06.

--- InterviewAIAgentAPI/InterviewAIAgentAPI/Models/TwilioSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InterviewAIAgentAPI.Models
-{
-    public class TwilioSettings
-    {
-        public string AccountSid { get; set; }
-        public string ApiKeySid { get; set; }
-        public string ApiKeySecret { get; set; }
-    }
-}
