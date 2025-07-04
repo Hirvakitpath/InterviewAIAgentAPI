6fe7bc6b3c533aae55bd60cc9d9dc048a8911d06
Jignesh Gohel <jigneshgohel.itpath@gmail.com>
2025-07-04 12:40:18 +0530

    - init commit

---
+++ InterviewAIAgentAPI/InterviewAIAgentAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using InterviewAIAgentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterviewAIAgentAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CandidateResume> Candidates { get; set; }
+        //public DbSet<Interview> Interviews { get; set; }
+        //public DbSet<Question> Questions { get; set; }
+        
+    }
+}
